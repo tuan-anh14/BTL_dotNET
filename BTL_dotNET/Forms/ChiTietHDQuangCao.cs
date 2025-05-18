27b6b9d077d11c63c1263ccbeb8b6398cfe90f15
Thieu Tuan Anh <thieutuananh14092k4@gmail.com>
2025-05-18 11:39:42 +0700

    Design form ChiTietHDQuangCao

---
+++ BTL_dotNET/Forms/ChiTietHDQuangCao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTL_dotNET.Forms
+{
+    public partial class ChiTietHDQuangCao : Form
+    {
+        public ChiTietHDQuangCao()
+        {
+            InitializeComponent();
+        }
+
+        private void ChiTietHDQuangCao_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
